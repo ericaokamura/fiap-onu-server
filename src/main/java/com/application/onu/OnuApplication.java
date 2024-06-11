99c903168a9628bd5ce5944fee3c12a4e836d0db
ericaokamura <erica.okamura@yahoo.com.br>
2024-06-11 15:31:06 -0300

    adicionando anotacao que habilita jpa repositories

--- src/main/java/com/application/onu/OnuApplication.java
+++ src/main/java/com/application/onu/OnuApplication.java
@@ -2,8 +2,10 @@ package com.application.onu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.application.onu.repository"})
 public class OnuApplication {
 
 	public static void main(String[] args) {
